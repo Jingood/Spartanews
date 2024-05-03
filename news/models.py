d085e1a06feec119d5a561e389d79b20d75d8908
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-03 15:37:35 +0900

    초기 모델 상태

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    url = models.CharField(max_length=150)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="news"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(autu_now=True)
