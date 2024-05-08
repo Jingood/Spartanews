2d3ce0a3bd605888d37881a1804feff950c21862
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-08 14:14:23 +0900

    오타 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'email',
-                  'created_at', 'introduction']
+                'created_at', 'introduction']
